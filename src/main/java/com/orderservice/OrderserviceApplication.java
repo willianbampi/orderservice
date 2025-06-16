50db629ae286209598f4f03f9945e6c06c218168
Willian Bampi <willian.bampi@gmail.com>
2025-06-16 16:44:40 -0300

    Configuration
    Change some configurations and rename files

--- src/main/java/com/orderservice/OrderserviceApplication.java
+++ src/main/java/com/orderservice/OrderserviceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrderserviceApplication {
+public class OrderServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OrderserviceApplication.class, args);
+		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 
 }
